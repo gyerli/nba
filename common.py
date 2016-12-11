055514432b9306d68d9b8b28f9e6a0fe5dc3c8b9
gyerli <gursoy@yerli.org>
2016-12-11 13:04:46 -0600

    global seetings for interoperability

--- common.py
+++ common.py
@@ -4,175 +4,183 @@ import psycopg2 as pg
 from sqlalchemy import create_engine
 
 
-
 class reg(object):
-  def __init__(self, cursor, row):
-    for (attr, val) in zip((d[0] for d in cursor.description), row) :
-      setattr(self, attr, val)
+    def __init__(self, cursor, row):
+        for (attr, val) in zip((d[0] for d in cursor.description), row):
+            setattr(self, attr, val)
 
 
 def start_run():
+    param_str = "season:{0};season_type:{1}".format(g_season, g_season_type)
 
-  param_str = "season:{0};season_type:{1}".format(g_season,g_season_type)
+    try:
+        cur = conn.cursor()
+        sql = "INSERT INTO job.run(start_dtm,run_status,parameters,season,season_type) " \
+              "VALUES ('{0}','{1}','{2}','{3}','{4}') " \
+              "RETURNING id".format(datetime.datetime.now(), 'IN PROGRESS', param_str, g_season, g_season_type)
+        cur.execute(sql)
+        run_id = cur.fetchone()[0]
+        log.debug('started run => {0}'.format(run_id))
+        conn.commit()
+    except Exception, e:
+        log.error('error creating session')
+        raise
 
-  try:
+    return run_id
+
+
+def end_run(run_id, status):
+    try:
+        cur = conn.cursor()
+        sql = "UPDATE job.run " \
+              "   SET run_status = '{0}', " \
+              "       end_dtm = '{1}' " \
+              " WHERE id = {2} ".format(status, datetime.datetime.now(), run_id)
+        cur.execute(sql)
+        log.debug('ended run => {0} with status => {1}'.format(run_id, status))
+        conn.commit()
+
+        if status == 'FAILED':
+            log.error('updating all run logs as FAILED')
+            sql = "UPDATE job.run_log " \
+                  "   SET node_status = 'FAILED', " \
+                  "       end_dtm = '{0}' " \
+                  " WHERE run_id = {1} ".format(datetime.datetime.now(), run_id)
+            log.debug(sql)
+            cur.execute(sql)
+            conn.commit()
+
+    except Exception, e:
+        log.error('error ending run')
+        raise
+
+
+def start_log(run_id, node, node_key, parent_key, node_status):
+    sql = "INSERT INTO job.run_log " \
+          "(run_id,node,node_key,parent_key,node_status,season,season_type,started_dtm) " \
+          "VALUES " \
+          "({0},'{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}')".format(
+          run_id
+        , node
+        , node_key
+        , parent_key
+        , node_status
+        , 'IN PROGRESS'
+        , g_season
+        , g_season_type
+        , datetime.datetime.now())
+
     cur = conn.cursor()
-    sql = "INSERT INTO job.run(start_dtm,run_status,parameters,season,season_type) " \
-          "VALUES ('{0}','{1}','{2}','{3}','{4}') " \
-          "RETURNING id".format(datetime.datetime.now(),'IN PROGRESS',param_str,g_season,g_season_type)
     cur.execute(sql)
-    run_id = cur.fetchone()[0]
-    log.debug('started run => {0}'.format(run_id))
     conn.commit()
-  except Exception, e:
-    log.error('error creating session')
-    raise
 
-  return run_id
 
-def end_run(run_id,status):
-  try:
+def end_log(run_id, node, key, status, group_status):
+    sql = "UPDATE job.run_log " \
+          "  SET  node_status = '{0}' " \
+          "       group_status = '{1}' " \
+          "      ,end_dtm = '{2}' " \
+          " WHERE run_id = {3} " \
+          "   AND node = '{4}' " \
+          "   AND node_key = '{5}' " \
+          "   AND season = '{6}' " \
+          "   AND season_type = '{7}' ".format(status, group_status, datetime.datetime.now(), run_id, node, key, g_season,
+                                               g_season_type)
+
     cur = conn.cursor()
-    sql = "UPDATE job.run " \
-          "   SET run_status = '{0}', " \
-          "       end_dtm = '{1}' " \
-          " WHERE id = {2} ".format(status,datetime.datetime.now(),run_id)
     cur.execute(sql)
-    log.debug('ended run => {0} with status => {1}'.format(run_id,status))
     conn.commit()
 
-    if status == 'FAILED':
-      log.error('updating all run logs as FAILED')
-      sql = "UPDATE job.run_log " \
-            "   SET node_status = 'FAILED', " \
-            "       end_dtm = '{0}' " \
-            " WHERE run_id = {1} ".format(datetime.datetime.now(),run_id)
-      log.debug(sql)
-      cur.execute(sql)
-      conn.commit()
-
-  except Exception, e:
-    log.error('error ending run')
-    raise
-
-
-
-def start_log(run_id,node,node_key,node_status):
-  sql = "INSERT INTO job.run_log " \
-        "(run_id,node,node_key,node_status,season,season_type,started_dtm) " \
-        "VALUES " \
-        "({0},'{1}','{2}','{3}','{4}','{5}','{6}')".format(
-         run_id
-        ,node
-        ,node_key
-        ,node_status
-        ,g_season
-        ,g_season_type
-        ,datetime.datetime.now())
-
-  cur = conn.cursor()
-  cur.execute(sql)
-  conn.commit()
-
-def end_log(run_id,node,key,status):
-  sql = "UPDATE job.run_log " \
-        "  SET node_status = '{0}' " \
-        "      ,end_dtm = '{1}' " \
-        " WHERE run_id = {2} " \
-        "   AND node = '{3}' " \
-        "   AND node_key = '{4}' " \
-        "   AND season = '{5}' " \
-        "   AND season_type = '{6}' ".format(status,datetime.datetime.now(),run_id,node,key,g_season,g_season_type)
-
-  cur = conn.cursor()
-  cur.execute(sql)
-  conn.commit()
-   
 
 def get_season_dates():
-  sql = "SELECT start_date, end_date, is_current " \
-        "  FROM job.season " \
-        " WHERE season = '{0}'" \
-        "   AND season_type = '{1}'".format(g_season,g_season_type)
+    sql = "SELECT start_date, end_date, is_current " \
+          "  FROM job.season " \
+          " WHERE season = '{0}'" \
+          "   AND season_type = '{1}'".format(g_season, g_season_type)
 
-  cur = conn.cursor()
-  cur.execute(sql)
-  dt = cur.fetchone()
-  return [dt[0],dt[1],dt[2]]
+    cur = conn.cursor()
+    cur.execute(sql)
+    dt = cur.fetchone()
+    return [dt[0], dt[1], dt[2]]
+
 
 def get_measures(node):
-  cur = conn.cursor()
+    cur = conn.cursor()
 
-  sql = "SELECT node, endpoint, measure, table_name, active, measure_type, " \
-        "       measure_category, comments, available_year, pk, measure_scope " \
-        "  FROM job.node " \
-        " WHERE node = '{0}' ".format(node)
+    sql = "SELECT node, endpoint, measure, table_name, active, measure_type, " \
+          "       measure_category, comments, available_year, pk, measure_scope " \
+          "  FROM job.node " \
+          " WHERE node = '{0}' ".format(node)
 
-  cur.execute(sql)
-  return cur
-
-def s_to_sql(df,params):
-  df.columns = map(unicode.lower, df.columns)
-  df.rename(columns={'to':'tov'},inplace=True)
-  df.rename(columns={'stats_value':'_stats_value'},inplace=True)
-  df['_season'] = g_season
-  df['_season_type'] = g_season_type
-  df['_create_date'] = cmn.now()
-  df.to_sql(name=params['table_name'],con=engine,schema='lnd',if_exists='append',index=False)
-
-def p_to_sql(df,params):
-  df.columns = map(unicode.lower, df.columns)
-  df.rename(columns={'to':'tov'},inplace=True)
-  df.rename(columns={'stats_value':'_stats_value'},inplace=True)
-  df['_player_id'] = params['player_id']
-  df['_team_id'] = params['team_id']
-  df['_season'] = g_season
-  df['_season_type'] = g_season_type
-  df['_create_date'] = datetime.datetime.now()
-  df.to_sql(name=params['table_name'],con=engine,schema='lnd',if_exists='append',index=False)
+    cur.execute(sql)
+    return cur
 
 
-def g_to_sql(df,params):
-  df.columns = map(unicode.lower, df.columns)
-  df.rename(columns={'to':'tov'},inplace=True)
-  df['_game_id'] = params['game_id']
-  df['_season'] = g_season
-  df['_season_type'] = g_season_type
-  df['_create_date'] = datetime.datetime.now()
-  df.to_sql(name=params['table_name'],con=engine,schema='lnd',if_exists='append',index=False)
+# def s_to_sql(df,params):
+#   df.columns = map(unicode.lower, df.columns)
+#   df.rename(columns={'to':'tov'},inplace=True)
+#   df.rename(columns={'stats_value':'_stats_value'},inplace=True)
+#   df['_season'] = g_season
+#   df['_season_type'] = g_season_type
+#   df['_create_date'] = cmn.now()
+#   df.to_sql(name=params['table_name'],con=engine,schema='lnd',if_exists='append',index=False)
+
+def p_to_sql(df, params):
+    df.columns = map(unicode.lower, df.columns)
+    df.rename(columns={'to': 'tov'}, inplace=True)
+    df.rename(columns={'stats_value': '_stats_value'}, inplace=True)
+    df['_player_id'] = params['player_id']
+    df['_team_id'] = params['team_id']
+    df['_season'] = g_season
+    df['_season_type'] = g_season_type
+    df['_create_date'] = datetime.datetime.now()
+    df.to_sql(name=params['table_name'], con=engine, schema='lnd', if_exists='append', index=False)
 
 
-def t_to_sql(df,params):
-  df.columns = map(unicode.lower, df.columns)
-  df.rename(columns={'to':'tov'},inplace=True)
-  df['_team_id'] = params['team_id']
-  df['_season'] = g_season
-  df['_season_type'] = g_season_type
-  df['_create_date'] = datetime.datetime.now()
-  df.to_sql(name=params['table_name'],con=engine,schema='lnd',if_exists='append',index=False)
-
-def m_to_sql(df,params):
-  df.columns = map(unicode.lower, df.columns)
-  df.rename(columns={'to':'tov'},inplace=True)
-  df['_team_id'] = params['team_id']
-  df['_season'] = params['season']
-  df['_create_date'] = datetime.datetime.now()
-  df.to_sql(name=params['table_name'],con=engine,schema='lnd',if_exists='append',index=False)
+def g_to_sql(df, params):
+    df.columns = map(unicode.lower, df.columns)
+    df.rename(columns={'to': 'tov'}, inplace=True)
+    df['_game_id'] = params['game_id']
+    df['_season'] = g_season
+    df['_season_type'] = g_season_type
+    df['_create_date'] = datetime.datetime.now()
+    df.to_sql(name=params['table_name'], con=engine, schema='lnd', if_exists='append', index=False)
 
 
-def s_to_sql(df,params):
-  df.columns = map(unicode.lower, df.columns)
-  df.rename(columns={'to':'tov'},inplace=True)
-  df['_season'] = g_season
-  df['_season_type'] = g_season_type
-  df['_create_date'] = datetime.datetime.now()
-  df.to_sql(name=params['table_name'],con=engine,schema='lnd',if_exists='append',index=False)
+def t_to_sql(df, params):
+    df.columns = map(unicode.lower, df.columns)
+    df.rename(columns={'to': 'tov'}, inplace=True)
+    df['_team_id'] = params['team_id']
+    df['_season'] = g_season
+    df['_season_type'] = g_season_type
+    df['_create_date'] = datetime.datetime.now()
+    df.to_sql(name=params['table_name'], con=engine, schema='lnd', if_exists='append', index=False)
+
+
+def m_to_sql(df, params):
+    df.columns = map(unicode.lower, df.columns)
+    df.rename(columns={'to': 'tov'}, inplace=True)
+    df['_team_id'] = params['team_id']
+    df['_season'] = params['season']
+    df['_create_date'] = datetime.datetime.now()
+    df.to_sql(name=params['table_name'], con=engine, schema='lnd', if_exists='append', index=False)
+
+
+def s_to_sql(df, params):
+    df.columns = map(unicode.lower, df.columns)
+    df.rename(columns={'to': 'tov'}, inplace=True)
+    df['_season'] = g_season
+    df['_season_type'] = g_season_type
+    df['_create_date'] = datetime.datetime.now()
+    df.to_sql(name=params['table_name'], con=engine, schema='lnd', if_exists='append', index=False)
+
 
 def refresh_mviews():
-  sql = "SELECT job.refresh_mviews()"
-  cur = conn.cursor()
-  cur.execute(sql)
-  conn.commit()
+    sql = "SELECT job.refresh_mviews()"
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+
 
 valid_seasons = ['2016-17', '2015-16', '2014-15', '2013-14', '2012-13', '2011-12', '2010-11', '2009-10', '2008-09',
                  '2007-08', '2006-07', '2005-06']
@@ -184,12 +192,12 @@ data_folder = './data/'
 log_folder = './log/'
 
 # Database
-#==============================================================================
+# ==============================================================================
 conn = pg.connect(database="nba", user="ictsh", password="gyerli", host="aws-srv-1", port="5432")
 engine = create_engine('postgresql://ictsh:gyerli@aws-srv-1:5432/nba')
 
 # Logger
-#==============================================================================
+# ==============================================================================
 log = logging.getLogger('go')
 log.setLevel(logging.DEBUG)
 # create file handler which logs even debug messages
