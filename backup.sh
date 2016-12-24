bfc8dd28b2e56a9c0f3d71d752899a05dfea758d
Gursoy Yerli <gursoy@yerli.org>
2016-12-24 12:24:20 -0600

    backup

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Location to place backups.
+backup_dir="/home/ec2-user/work/nba/backup/"
+#String to append to the name of the backup files
+#backup_date=`date +%d-%m-%Y`
+backup_date=`date +%Y%m%d`
+#Numbers of days you want to keep copie of your databases
+number_of_days=5
+databases="nba"
+for i in $databases; do
+  if [ "$i" != "template0" ] && [ "$i" != "template1" ]; then
+    echo Dumping $i to $backup_dir$i\_$backup_date.dmp
+    pg_dump -U postgres -Fc $i > $backup_dir$i\_$backup_date.dmp
+    gzip $backup_dir$i\_$backup_date.dmp
+    echo compressing $backup_dir$i\_$backup_date.dmp
+  fi
+done
+find $backup_dir -type f -prune -mtime +$number_of_days -exec rm -f {} \;
