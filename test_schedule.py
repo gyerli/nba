9067a06a6ec71f08dbcf52827fd5d64b0c2b9e85
gyerli <gursoy@yerli.org>
2016-12-28 14:04:12 -0600

    json

---
+++ test_schedule.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import json
+from requests import get
+
+url = 'http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2016/league/00_full_schedule.json'
+
+_json = get(url).json()
+_df = pd.DataFrame(_json)
+
+x = json.loads(_js)
+
+for i in _df:
+    print i
